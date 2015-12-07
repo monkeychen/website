a47c8d87f2bd1d3b0c87460c25d988c912149ac3
monkeychen <cza55007@gmail.com>
2015-12-07 08:01:32 +0800

    修改timecart footer相关页面

--- timecart/catalog/language/zh-HK/common/footer.php
+++ timecart/catalog/language/zh-HK/common/footer.php
@@ -15,6 +15,6 @@ $_['text_account']      = '會員中心';
 $_['text_order']        = '曆史訂單';
 $_['text_wishlist']     = '收藏列表';
 $_['text_newsletter']   = '訂閱咨詢';
-$_['text_powered']      = '技術支持 <a href="http://www.mycncart.com">MyCnCart</a><br /> %s &copy; %s';
+$_['text_powered']      = '%s &copy; %s';
 
 
