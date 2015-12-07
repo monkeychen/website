53645c00994028135456b09ae9db250ce08ea2b2
monkeychen <cza55007@gmail.com>
2015-12-07 23:00:51 +0800

    modify git ignore files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,12 @@
     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <meta name="description" content="">
     <meta name="author" content="">
+    <link rel="icon" href="/static/images/icon.png">
 
     <title>Simiam's site</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/static/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <style>
@@ -52,13 +53,26 @@
             </button>
             <a class="navbar-brand" href="http://simiam.com"><img class="logo_img" src="/static/images/logo.png"></a>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="http://blog.simiam.com" target="_blank">博客</a></li>
-                <li><a href="http://100.simiam.com" target="_blank">教研平台</a></li>
-                <li><a href="http://timecart.simiam.com" target="_blank">商城演示</a></li>
-            </ul>
-        </div>
+        <ul class="nav navbar-nav navbar-left">
+            <li><a href="http://simiam.com">首页</a></li>
+            <li role="presentation" class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                   aria-haspopup="true" aria-expanded="false">应用 <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li ><a href="http://blog.simiam.com" target="_blank">博客</a></li>
+                    <li><a href="http://100.simiam.com" target="_blank">教研平台</a></li>
+                    <li><a href="http://timecart.simiam.com" target="_blank">商城演示</a></li>
+                </ul>
+            </li>
+        </ul>
+        <form class="navbar-form navbar-right" role="search">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="搜索">
+            </div>
+            <button type="submit" class="btn btn-default">
+                <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+            </button>
+        </form>
         <!--/.nav-collapse -->
     </div>
 </nav>
@@ -76,7 +90,7 @@
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
-<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="/static/js/jquery/jquery-1.11.3.min.js"></script>
+<script src="/static/js/bootstrap/bootstrap.min.js"></script>
 </body>
 </html>
